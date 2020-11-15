3dd8f19aaecfba9312413bce07eb7296d1fdfe69
Jad Mintun <jadmintun@gmail.com>
2020-11-15 09:28:22 -0500

    remove files

--- src/global-styles.js
+++
@@ -1,9 +0,0 @@
-import { injectGlobal } from "styled-components";
-
-injectGlobal`
-@import url('https://fonts.googleapis.com/css?family=Noto+Sans');
-  body {
-    margin: 0px;
-    font-family: 'Noto Sans';
-  }
-`;
